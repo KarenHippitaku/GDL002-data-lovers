9100ca7a1c923b48e1f749a005bfb7b142abd2e6
KarenHippitaku <hippitaku@gmail.com>
2019-03-12 06:44:58 -0600

    Segunda iteración de grid, test y funciones   -Grid con fallas menores, responsive parcial   -Test con 100% de covertura   -Funciones de data.js sin elementos del DOM   -Se eliminaron identificadores en español   -Probado en mozilla

--- src/main.js
+++ src/main.js
@@ -1,66 +1,130 @@
-const mostrarData = () => {
-  document.getElementById("infoBlock").style.display = "block";
- };
-  document.getElementById("Enter").addEventListener("click", mostrarData);
- 
- const showInjuriesFilter = () => {
-    let buttonInjuriesList = document.getElementById("totalData");
-    window.data.fillElements(INJURIES, buttonInjuriesList);
- };
- 
- const showInjuriesOrder = () => {
-  let buttonInjuriesList = document.getElementById("totalData");
-  window.data.fillElementsOrder(INJURIES, buttonInjuriesList);
-};
- 
 
-const filter = () =>{
-  document.getElementById("totalData").innerHTML = '';
-   const values = document.getElementById("filterMenu").value;
-   switch(values){
-    case "filterForYear":
-      mostrarData();
-      showInjuriesFilter();
-      activateButtonFunction();
-      break;
-   // case "filterForRisk":
- 
+const showData = () => {
+  document.getElementById("welcomeBlock").style.display = "none";
+  document.getElementById("idButton").style.display = "block";
+  document.getElementById("infoBlock").style.display = "grid";
+ };
+  document.getElementById("enterButton").addEventListener("click", showData);
+  document.getElementById("registerButton").addEventListener("click", showData);
+
+  const showButtons = () => {
+     let buttonInjuriesList = document.getElementById("totalData");
+     const arrayYearFilter = window.data.filterData(INJURIES);
+     for(let i=0; i<arrayYearFilter.length; i++){
+       let spanInjurie = document.createElement("span");
+       spanInjurie.id = "Injurie" + [i];
+       let buttonInjurie = document.createElement("button");
+       buttonInjurie.value = arrayYearFilter[i];
+       spanInjurie.appendChild(buttonInjurie);
+       spanInjurie.innerHTML =` <button name="button" value = "${arrayYearFilter[i]}-01-04">${arrayYearFilter[i]}</button>`;
+       buttonInjuriesList.insertAdjacentElement("beforeend", spanInjurie);
+     }
+
+  };
+
+  const activateButtonFunctionFilterOrder = (INJURIES) =>{
+   let activateButton = document.getElementsByName("button");
+   let arrayButton = Array.from(activateButton);
+   arrayButton.forEach(function (element) {
+     element.addEventListener("click",(event)=>{
+       elementsOfDOM();
+       const valueButton = event.target.value;
+       let showNumber = window.data.ShowNumbers(INJURIES, valueButton);
+       return showNumber.forEach((element)=>{
+         document.getElementById("Result").innerHTML += element + `<br />`;
+       });
+     });
+   });
+ };
+
+ const filter = () =>{
+   document.getElementById("totalData").innerHTML = '';
+    const values = document.getElementById("filterMenu").value;
+    switch(values){
+     case "filterForYear":
+       mostrarData();
+       showButtons();
+       activateButtonFunctionFilterOrder(INJURIES);
+       break;
+    // case "filterForRisk":
+
+    }
+ };
+
+ document.getElementById("filterMenu").addEventListener("change",filter);
+
+  const showButtonsOrder = () => {
+   let buttonInjuriesList = document.getElementById("totalData");
+   const arrayYearFilter = window.data.fillElementsOrder(INJURIES, buttonInjuriesList);
+   for(let i=0; i<arrayYearFilter.length; i++){
+     let spanInjurie = document.createElement("span");
+     spanInjurie.id = "Injurie" + [i];
+     let buttonInjurie = document.createElement("button");
+     buttonInjurie.value = arrayYearFilter[i];
+     spanInjurie.appendChild(buttonInjurie);
+     spanInjurie.innerHTML =` <button name="button" value = "${arrayYearFilter[i]}-01-04">${arrayYearFilter[i]}</button>`;
+     buttonInjuriesList.insertAdjacentElement("beforeend", spanInjurie);
    }
-};
-
-document.getElementById("filterMenu").addEventListener("change",filter);
-
-const order = () =>{
-  document.getElementById("totalData").innerHTML = '';
-   const values = document.getElementById("orderMenu").value;
-   switch(values){
-    case "lastToFirst":
-      mostrarData();
-      showInjuriesOrder(INJURIES);
-      activateButtonFunction();
-      break;
-    case "FirstToLast":
-      mostrarData();
-      showInjuriesFilter();
-      activateButtonFunction();
-      break;
-    case "default":
-     alert("No has elegido ninguno");
-   }
-};
-
-document.getElementById("orderMenu").addEventListener("change",order);
-
- const closeModal = () =>{
-   document.getElementById("closeModal").style.display = "none";
-   document.getElementById("Resultado").style.display = "none";
+   activateButtonFunctionFilterOrder(INJURIES);
  };
- document.getElementById("closeModal").addEventListener("click",closeModal);
 
-const activateButtonFunction = () =>{
-  let activateButton = document.getElementsByName("button");
-  let arrayButton = Array.from(activateButton);
-  arrayButton.forEach(function (element) {
-    element.addEventListener("click",window.data.mostrarNumeros);
-  });
-};
+ const sort = () =>{
+   document.getElementById("totalData").innerHTML = '';
+    const values = document.getElementById("sortMenu").value;
+    switch(values){
+     case "lastToFirst":
+       mostrarData();
+       showButtonsOrder(INJURIES);
+       break;
+     case "FirstToLast":
+       mostrarData();
+       showButtons();
+       activateButtonFunctionFilterOrder(INJURIES);
+       break;
+     case "default":
+      alert("No has elegido ninguno");
+    }
+ };
+
+ document.getElementById("sortMenu").addEventListener("change", sort);
+
+ const calculate = () =>{
+   document.getElementById("totalData").innerHTML = '';
+    const values = document.getElementById("calculusMenu").value;
+    switch(values){
+     case "YearWithFewerInjuries":
+       mostrarData();
+       showButtons();
+       activateButtonFunctionToCalculate();
+       break;
+    // case "filterForRisk":
+
+    }
+ };
+
+ document.getElementById("calculusMenu").addEventListener("change",calculate);
+
+  const closeModal = () =>{
+    document.getElementById("closeModal").style.display = "none";
+    document.getElementById("Result").style.display = "none";
+  };
+  document.getElementById("closeModal").addEventListener("click",closeModal);
+
+ const elementsOfDOM = () =>{
+   document.getElementById("Result").innerHTML = "";
+   document.getElementById("closeModal").style.display = "block";
+   document.getElementById("Result").style.display = "block";
+ };
+
+ const activateButtonFunctionToCalculate = () =>{
+   let activateButton = document.getElementsByName("button");
+   let arrayButton = Array.from(activateButton);
+   arrayButton.forEach(function (element) {
+     element.addEventListener("click", (event)=>{
+       elementsOfDOM();
+       const valueButton = event.target.value;
+       const averacheDate = window.data.showNumbersOfCalculate(INJURIES, valueButton);
+       document.getElementById("Result").innerHTML += "El promedio de accidentes en el año " + valueButton + " es de " + averacheDate + `<br />`;
+     });
+   });
+ };
--- test/data.spec.js
+++ test/data.spec.js
@@ -20,20 +20,26 @@ describe('injuries', () => {
   });
 });
 
-describe('filterByYear', () => {
+describe('ShowNumbers', () => {
   it('must be a function', () => {
-    expect(data.filterByYear(INJURIES)).toBe('function');
+    expect( window.data.ShowNumbers(INJURIES, "2000-01-04") instanceof Array).toBe(true);
   });
 });
 
-describe('filterByInjurie', () => {
-  it('must be a function', () => {
-    expect(data.filterByInjurie(INJURIES)).toBe('function');
+describe('filterData', () => {
+  it('must be a function of filter years', () => {
+    expect(data.filterData(INJURIES)).toEqual([2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]);
   });
 });
 
-describe('sortAlphabetically', () => {
-  it('must be a function', () => {
-    expect(data.sortAlphabetically(INJURIES)).toBe('function');
+describe('filterElementsOrder', () => {
+  it('must be a function of order years', () => {
+    expect(data.fillElementsOrder(INJURIES)).toEqual([2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000]);
+  });
+});
+
+describe('showNumbersOfCalculate', () => {
+  it('must be a function of calculate averache', () => {
+    expect(data.showNumbersOfCalculate(INJURIES, "2000-01-04") instanceof Number).toBe(false);
   });
 });
